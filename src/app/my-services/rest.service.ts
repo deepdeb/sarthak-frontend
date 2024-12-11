11f4bd7ebc7d0e45e3b036d9b40088fe46d039fd
deepdeb <debnath.pradeep619@gmail.com>
2024-12-11 12:17:32 +0530

    modify files by PRADEEP

--- src/app/my-services/rest.service.ts
+++ src/app/my-services/rest.service.ts
@@ -11,8 +11,8 @@ const httpOptions = {
 })
 export class RestService {
 
-  // API_ROOT = "https://sarthakcomponents.com/";
-  API_ROOT = "http://localhost:3000/";
+  API_ROOT = "https://sarthakcomponents.com/";
+  // API_ROOT = "http://localhost:3000/";
 
   constructor(private http: HttpClient) { }
 
