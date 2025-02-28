28a56f908edbf63a8b125bdcfc61c767e18c1ffd
deepdeb <debnath.pradeep619@gmail.com>
2025-02-28 18:03:20 +0530

    minor change in rest service

--- src/app/my-services/rest.service.ts
+++ src/app/my-services/rest.service.ts
@@ -11,9 +11,9 @@ const httpOptions = {
 })
 export class RestService {
 
-  // API_ROOT = "https://103.184.242.66:8088/";
+  API_ROOT = "https://sarthakcomponents.com:8088/";
   // API_ROOT = "https://sarthakcomponents.com/";
-  API_ROOT = "http://localhost:3000/";
+  // API_ROOT = "http://localhost:3000/";
 
   constructor(private http: HttpClient) { }
 
